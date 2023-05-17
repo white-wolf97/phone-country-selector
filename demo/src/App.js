b4f776d0697ae20f03624b2c8fc8d7d6eed12f1a
Adolfo Castelo <adolfocastelo99@gmail.com>
2023-05-17 17:19:17 -0300

    feat: demo project

---
+++ demo/src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PhoneCountrySelector from "@practiauy/phone-country-selector";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <PhoneCountrySelector />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PhoneCountrySelector = () => {
+  return (
+	<div>Yaaay! We are on NPM!</div>
+  )
+}
+
+export default PhoneCountrySelector
